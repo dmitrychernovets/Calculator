86c538d4850c0a67661016ed5c7ef0b1438c12d2
charnavetsdmitry <838-30-03@tut.by>
2022-02-03 22:14:20 +0300

    fix formatting

--- calculator/Number.swift
+++ calculator/Number.swift
@@ -2,11 +2,11 @@ final class Number {
     
     private var numberValue = ""
     
-    var arrrrr : Dictionary<String, ()->Int> = ["+" : {2+7}]
+    var arrrrr : Dictionary<String, () -> Int> = ["+" : {2+7}]
     
     func addToNumberValue(_ point:String){numberValue += point}
     
-    func getNumberValue() ->String {numberValue}
+    func getNumberValue() -> String {numberValue}
     
     func setNumberValue(_ number: String){numberValue = number}
 }
--- calculator/simpleCalculator.swift
+++ calculator/simpleCalculator.swift
@@ -17,13 +17,13 @@ class SimpeCalculator {
     // после -> пробел
     // я бы сделала так func setNumbers(_ number: String) ->String { и тогда при вызове красиво
     // тоже мб как на гард заменить уберет вложенность, суть останется такой же, но так предпочитают больше
-    func setNumbers(_ number: String) ->String {
+    func setNumbers(_ number: String) -> String {
         getCurrentNumber().addToNumberValue(number)
         return allLine()
     }
     // мб вынести в отдельную функцию то что внутри первого иф оно в двух практически одинаковое и даже мб по тернарному оператору тут
     // все не очень любят так много вложенностей и if else так что мб некоторые аменить на гард и будет проще выглядеть, не так пугающе
-    func setPoint() ->String {
+    func setPoint() -> String {
         let number = getCurrentNumber()
         let value = number.getNumberValue()
         
@@ -35,7 +35,7 @@ class SimpeCalculator {
         return allLine()
     }
     
-    func setOperator(_ oper: String) ->String {
+    func setOperator(_ oper: String) -> String {
             // без пробела перед :
             // оперделять так по тегу не лучшее решение, считается непонятный набор цифр, но вообще как по мне норм, к тому это в одном месте
             // но мб попробуем сделать enum с операторами и по тексту кнопки определять какой оператор, но вообще а надо оно? если просто текст кнопки подсьавлять в выражение не будет тоже самое?
@@ -43,20 +43,20 @@ class SimpeCalculator {
         return allLine()
     }
     
-    func calculate() ->String {
+    func calculate() -> String {
         // не надо пробел после Double
         let first = Double(firstNumber.getNumberValue()) ?? 0.0
         let second = Double(secondNumber.getNumberValue()) ?? 0.0
         
         // а вот тут уже можно сделать enum и по rawValue опредялть какой оператор, просто сипатичнее и в будущем можно переиспользовать лиюо в setOperator можно сразу ставить тип из enum в pressedOperator и по типу смотреть
         switch pressedOperator {
-        case Operations.division.rawValue :
+        case Operations.division.rawValue:
             return "\(first / second)"
-        case Operations.muitiplication.rawValue :
+        case Operations.muitiplication.rawValue:
             return "\(first * second)"
-        case Operations.substraction.rawValue :
+        case Operations.substraction.rawValue:
             return "\(first - second)"
-        case Operations.addition.rawValue :
+        case Operations.addition.rawValue:
             return "\(first + second)"
         default:
             return "error"
@@ -65,16 +65,16 @@ class SimpeCalculator {
     
     // лучше использовать так Double(firstNumber)?.squareRoot(), Darwin это вообще либа которая работает с ядром айфоном, предпочитают ей так на прямую не пользоваться (если что за 5 минут как посмотреть эту часть мне первый раз про нее рассказал Даник хаха)
     // принудительно лучше не разворачивать, потому что в случае не успеха будет краш, просто приложение вылетит, я бы через гард проверила
-    func sqrt() ->String {String(Double(firstNumber.getNumberValue())?.squareRoot() ?? 0.0)}
+    func sqrt() -> String {String(Double(firstNumber.getNumberValue())?.squareRoot() ?? 0.0)}
     
-    func clearAll() ->String {
+    func clearAll() -> String {
         firstNumber.setNumberValue("")
         secondNumber.setNumberValue("")
         pressedOperator = ""
         return "0"
     }
     
-    private func getCurrentNumber() ->Number {pressedOperator.isEmpty ? firstNumber : secondNumber}
+    private func getCurrentNumber() -> Number {pressedOperator.isEmpty ? firstNumber : secondNumber}
     
-    private func allLine() ->String {firstNumber.getNumberValue() + pressedOperator + secondNumber.getNumberValue()}
+    private func allLine() -> String {firstNumber.getNumberValue() + pressedOperator + secondNumber.getNumberValue()}
 }
